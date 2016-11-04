5f7352491e4feb13d04ba9bc6375980bc14fb39f
Miguel Moctezuma <red_spider2010@hotmail.com>
2016-11-03 18:53:17 -0700

    Se agregaron las clases
    se agrego clases

---
+++ Air-Combat/eneAvion.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class eneAvion here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class eneAvion extends Actor
+{
+    /**
+     * Act - do whatever the eneAvion wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
