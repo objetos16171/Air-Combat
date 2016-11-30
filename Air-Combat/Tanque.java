daa0ecdce78528cb317c275970e9838d26513c15
Reaper25 <Sanchez@MacBook-Air-de-Micky.local>
2016-11-30 15:40:06 -0600

    Se creo enemigo tanque
    Se agrego un nuevo enemigo

---
+++ Air-Combat/Tanque.java
@@ -0,0 +1,89 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Tanque here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tanque extends Enemigos
+{
+   private int tam = 10;
+    private int velX;
+    private int velY = 2;
+    private int vida = 0;
+    private SimpleTimer reloj;
+    /**
+     * Act - do whatever the eneAvion wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public Tanque()
+    {
+        setLocation(Greenfoot.getRandomNumber(480), 0);
+        reloj = new SimpleTimer();
+    }
+    
+    public void act() 
+    {
+        muevete();
+        if(reloj.millisElapsed() > 1000){
+           disparaEnemigo();
+           reloj.mark();
+        }
+        elimina();
+    }
+    
+    public void muevete()
+    {
+       setLocation(getX() + velX, getY() + velY); 
+       if(getX()<tam||getX()>=480-tam)
+       {
+          velX=-1*velX;
+       }
+            
+       //if(getY()<=tam||getY()>=480-tam)
+       //{
+       //  World mundo = getWorld();
+       //  getWorld().removeObject(this);
+       //}
+    }
+    
+    public void elimina()
+    {
+      if(getY()<=tam||getY()>=480-tam)
+      {
+         World mundo = getWorld();
+         getWorld().removeObject(this); 
+      }  
+      else if(isTouching(Bala.class))
+      {
+        vida++;
+        World mundo = getWorld();
+        if(vida==1)
+        {
+         Explocion explo = new Explocion(); //se crea una explocion
+         setImage(explo.getImage()); //pone una explocion
+         Greenfoot.delay(1); //se realiza un delay para visualizar explosion
+         getWorld().removeObject(this);
+        }
+      }
+      else if(isTouching(Bomba.class)) 
+      {
+         World mundo = getWorld();
+         Explocion explo = new Explocion(); //se crea una explocion
+         setImage(explo.getImage()); //pone una explocion
+         Greenfoot.delay(1); //se realiza un delay para visualizar explosion
+         getWorld().removeObject(this);
+      }
+    }
+    public void disparaEnemigo()
+    {
+            GreenfootImage image = getImage();
+            BalaE balaE = new BalaE();
+            World mundo = getWorld();
+            ((World)mundo).addObject(balaE, getX(), getY()+100);
+            //Greenfoot.delay(1);
+            setImage(image); 
+    }
+}
