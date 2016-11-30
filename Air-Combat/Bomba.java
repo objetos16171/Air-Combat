db06cba1de9945ff8fadb0f24e9c7808fdc201b1
jhidarthy <castillo.master7@gmail.com>
2016-11-30 17:56:49 -0600

    Se le agrego explosion a la bomba
    La bomba explota al llegar al borde de la pantalla, o al estar cerca de
    ella o en devido caso explota cuando toca a un enemigo

--- Air-Combat/Bomba.java
+++ Air-Combat/Bomba.java
@@ -16,16 +16,24 @@ public class Bomba extends Actor
     
     public void act() 
     {
-        mueve();
+        mueveyExplota();
     }
     
-    public void mueve()
+    public void mueveyExplota()
     {
         setLocation(getX(), getY()-velBomb);
-        if(isAtEdge())
+        if(getY() == 150 || isTouching(Enemigos.class)||isAtEdge())
         {
+            GreenfootImage image = getImage(); //guarda la imagen actual del avion para que no se pierda
+            
+            fase2 f2 = new fase2(); ///se crea nuevo objero fase2
+            setImage( f2.getImage());//se reemplaza la imagen por la de fase 2
+            fase3 f3 = new fase3(); ///se crea nuevo objero fase3
+            setImage( f3.getImage());//se reemplaza la imagen por la de fase 3
+            Greenfoot.delay(2); //delay para ver la animacion
             World mundo = getWorld();
             ((Mundo1)mundo).removeObject(this);
+            
         }
     }
 }
---
+++ Air-Combat/fase3.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class fase3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class fase3 extends Bomba
+{
+    /**
+     * Act - do whatever the fase3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
