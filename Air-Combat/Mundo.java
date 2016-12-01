5e5e8af0ab81e085d81fd3ca504a4bbaa85b80d9
jhidarthy <castillo.master7@gmail.com>
2016-11-30 19:29:17 -0600

    -

---
+++ Air-Combat/Mundo.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Mundo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mundo extends World
+{
+
+    /**
+     * Constructor for objects of class Mundo.
+     * 
+     */
+    public Mundo()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(480, 600, 1); 
+    }
+}
